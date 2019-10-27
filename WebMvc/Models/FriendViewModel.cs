7e138ccb650f7e0b5c798f143e226635c0a3a493
Carlos Henrique <carlos.hrq.rodrigues@gmail.com>
2019-10-27 17:01:43 -0300

    view model

---
+++ WebMvc/Models/FriendViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebMvc.Models {
+    public class FriendViewModel {
+        public int Id {
+            get; set;
+        }
+
+        public string Name {
+            get; set;
+        }
+
+        public string LastName {
+            get; set;
+        }
+
+        public string Email {
+            get; set;
+        }
+
+        public DateTime BirthDate {
+            get; set;
+        }
+
+        public FriendViewModel(int Id, string Name, string LastName, string Email, DateTime BirthDate) {
+            this.Id = Id;
+            this.Name = Name;
+            this.LastName = LastName;
+            this.Email = Email;
+            this.BirthDate = BirthDate;
+        }
+    }
+}
