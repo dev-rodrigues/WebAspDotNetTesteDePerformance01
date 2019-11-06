8acf54a340448f1dd9a0e0f8956d930300aaaea9
carlos henrique <carlos.hrq.rodrigues@gmail.com>
2019-11-06 19:13:00 -0300

    relacionando

---
+++ Core/Model/Toy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Model {
+    public class Toy {
+        public int Id {
+            get; set;
+        }
+
+        public string Name {
+            get; set;
+        }
+
+        public virtual Friend Friend {
+            get; set;
+        }
+
+        [ForeignKey("Friend")]
+        public int IdFriend {
+            get; set;
+        }
+    }
+}
